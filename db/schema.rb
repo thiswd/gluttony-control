e813c4aa00011e29d9dbfbf08cffdebaf7afe65a
Thiago Sawada <tms1_tms2@hotmail.com>
2023-09-21 14:53:22 -0300

    refactor: 🔄 Modify data type of 'code' column in Products table   - Updated the data type from integer to string via migration `20230920022751_change_code_type_in_products.rb`.   - Ensured backward compatibility with a down method to revert changes if needed.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_195009) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_022751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_195009) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.integer "code", null: false
+    t.string "code", null: false
     t.string "status"
     t.datetime "imported_t"
     t.string "url"
