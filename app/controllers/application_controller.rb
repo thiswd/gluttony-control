f7d64891fcff91d4c7eefa2fa832fd6a0455fad0
Thiago Sawada <tms1_tms2@hotmail.com>
2023-09-22 18:59:19 -0300

    refactor: move system status to separate controller
    - Extract system status functionality from ApplicationController
    - Introduce SystemStatusController with `show` action
    - Update routes to point to SystemStatusController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,6 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-  def status
-    db_status = DbStatusChecker.check
-    last_cron_run = ImportHistory.last.formatted_imported_at
-    uptime = SystemStatusChecker.uptime
-    memory_usage = SystemStatusChecker.formatted_memory_usage
-
-    render json: {
-      db_status:,
-      last_cron_run:,
-      uptime:,
-      memory_usage:
-    }
-  end
-
   private
 
     def record_not_found
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  get "/", to: "application#status"
+
+  get "/", to: "system_status#show"
+
   resources :products, param: :code, only: [:index, :show, :update, :destroy]
 end
