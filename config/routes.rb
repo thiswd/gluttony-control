6421b80ae86fc5dd2531d768380b61d0f7753605
Thiago Sawada <tms1_tms2@hotmail.com>
2023-09-22 18:37:08 -0300

    feat: integrate Rswag for API documentation
    - Add `rswag-api` and `rswag-ui` gems
    - Create `rswag` views, initializers, and configuration
    - Add routes for `Rswag` engines
    - Include initial Swagger YAML for API V1 documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   get "/", to: "application#status"
   resources :products, param: :code, only: [:index, :show, :update, :destroy]
 end
