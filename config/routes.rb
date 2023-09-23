d021d2a4e610e15b73fd72485c7eef0598829208
Thiago Sawada <tms1_tms2@hotmail.com>
2023-09-22 21:20:14 -0300

    refactor: Rubocop yellow flags

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
 
   get "/", to: "system_status#show"
 
