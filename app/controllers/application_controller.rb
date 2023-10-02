66d6be25e728cdabf1df5ee78fc4aff13856df56
Thiago Sawada <tms1_tms2@hotmail.com>
2023-10-02 00:17:52 -0300

    feat: implement API key-based authentication
    - Add an authentication callback in `ApplicationController` to verify the provided API key.
    - Introduce `ApiKey` model to manage and generate unique access tokens.
    - Provide a rake task to generate new API keys for client usage.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :authenticate
 
   private
 
     def record_not_found
       render json: { error: "Record not found" }, status: :not_found
+    end
+
+    def authenticate
+      api_key = request.headers['X-API-KEY']
+
+      unless ApiKey.exists?(access_token: api_key)
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
     end
 end
