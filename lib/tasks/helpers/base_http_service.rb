7d5defbc59d2103c68e51c7758475b9112e24325
Thiago Sawada <tms1_tms2@hotmail.com>
2023-10-19 15:58:18 -0300

    refactor: improve error handling and response validation in BaseHttpService
    - Extracted response validation into a separate method `validate_response` for cleaner code.
    - Moved all error handling logic to a new method `handle_error` to centralize exception management.

--- lib/tasks/helpers/base_http_service.rb
+++ lib/tasks/helpers/base_http_service.rb
@@ -3,17 +3,28 @@ require "net/http"
 class BaseHttpService
   def self.fetch_content(uri)
     response = Net::HTTP.get_response(uri)
-
-    raise "Failed to fetch from #{uri}" unless response.is_a?(Net::HTTPSuccess)
-
+    validate_response(response, uri)
     response.body
-  rescue Net::HTTPClientError, Net::HTTPServerError => e
-    raise "An HTTP error occurred: #{e.message}"
-  rescue Net::OpenTimeout
-    raise "Network open timeout for #{uri}."
-  rescue Timeout::Error
-    raise "Request to #{uri} timed out."
-  rescue StandardError => e
-    raise "An error occurred while fetching from #{uri}: #{e.message}"
+  rescue => e
+    handle_error(e, uri)
   end
+
+  private
+
+    def self.validate_response(response, uri)
+      raise "Failed to fetch from #{uri}" unless response.is_a?(Net::HTTPSuccess)
+    end
+
+    def self.handle_error(e, uri)
+      case e
+      when Net::HTTPClientError, Net::HTTPServerError
+        raise "An HTTP error occurred: #{e.message}"
+      when Net::OpenTimeout
+        raise "Network open timeout for #{uri}."
+      when Timeout::Error
+        raise "Request to #{uri} timed out."
+      else
+        raise "An error occurred while fetching from #{uri}: #{e.message}"
+      end
+    end
 end
