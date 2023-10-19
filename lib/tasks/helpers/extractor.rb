229e24675c52cdd6af5559a253ff551fdeff4624
Thiago Sawada <tms1_tms2@hotmail.com>
2023-10-19 16:06:10 -0300

    refactor: improve file validation and error handling in Extractor
    - Introduced the `validate_file` method for better file validation.
    - Moved extraction logic to `extract_content` for improved readability.
    - Centralized error handling in the `handle_error` method.

--- lib/tasks/helpers/extractor.rb
+++ lib/tasks/helpers/extractor.rb
@@ -2,21 +2,41 @@ require "zlib"
 
 class Extractor
   def self.extract(gz_file_path, destination, records_amount)
-    raise "Source file #{gz_file_path} does not exist" unless File.exist?(gz_file_path)
+    validate_file(gz_file_path)
+    extract_content(gz_file_path, destination, records_amount)
+  rescue => e
+    handle_error(e, gz_file_path)
+  end
 
-    Zlib::GzipReader.open(gz_file_path) do |gz|
-      File.open(destination, "wb") do |file|
-        records_amount.times do
-          line = gz.readline
-          file.write(line)
-        rescue EOFError
-          break
+  private
+
+    def self.validate_file(gz_file_path)
+      raise "Source file #{gz_file_path} does not exist" unless File.exist?(gz_file_path)
+    end
+
+    def self.extract_content(gz_file_path, destination, records_amount)
+      Zlib::GzipReader.open(gz_file_path) do |gz|
+        File.open(destination, "wb") do |file|
+          records_amount.times do
+            begin
+              line = gz.readline
+              file.write(line)
+            rescue EOFError
+              break
+            end
+          end
         end
       end
     end
-  rescue Zlib::GzipFile::Error
-    raise "The file #{gz_file_path} is not a valid gzip file or is corrupted"
-  rescue IOError => e
-    raise "An IO error occurred: #{e.message}"
-  end
+
+    def self.handle_error(e, gz_file_path)
+      case e
+      when Zlib::GzipFile::Error
+        raise "The file #{gz_file_path} is not a valid gzip file or is corrupted"
+      when IOError
+        raise "An IO error occurred: #{e.message}"
+      else
+        raise "An unknown error occurred: #{e.message}"
+      end
+    end
 end
